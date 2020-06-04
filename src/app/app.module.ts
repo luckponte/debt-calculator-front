9c8ee84e206f9cdb4c379bca88293ad7f0ac514a
luckponte <luckpontecorreia@gmail.com>
2020-06-04 18:48:53 -0300

    Add Brazilian Portuguese Localization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [

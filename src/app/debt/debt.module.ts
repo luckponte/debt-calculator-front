26a7a7ea9b9f26084cfa9862497eb581cf440421
luckponte <luckpontecorreia@gmail.com>
2020-06-04 20:31:03 -0300

    Remove redundant importation

--- src/app/debt/debt.module.ts
+++ src/app/debt/debt.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Debt } from '../debt.type';
 import { DebtRoutingModule } from './debt-routing.module';
 import { DebtComponent } from './debt.component';
 import { DebtCardComponent } from './debt-card/debt-card.component';

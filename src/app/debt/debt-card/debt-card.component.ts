73f9cb4d8b4799faef63e1ac3437d335d44839c8
luckponte <luckpontecorreia@gmail.com>
2020-06-04 16:00:07 -0300

    Create debt module components

---
+++ src/app/debt/debt-card/debt-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-debt-card',
+  templateUrl: './debt-card.component.html',
+  styleUrls: ['./debt-card.component.css']
+})
+export class DebtCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/debt/debt.module.ts
+++ src/app/debt/debt.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DebtComponent } from './debt.component';
-
-
+import { DebtCardComponent } from './debt-card/debt-card.component';
+import { DebtDetailComponent } from './debt-detail/debt-detail.component';
+import { DebtListComponent } from './debt-list/debt-list.component';
 
 @NgModule({
-  declarations: [DebtComponent],
+  declarations: [DebtComponent, DebtCardComponent, DebtDetailComponent, DebtListComponent],
   imports: [
     CommonModule
   ]

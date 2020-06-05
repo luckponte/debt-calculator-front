49919d101e73c427e428c23025824647c404abae
luckponte <luckpontecorreia@gmail.com>
2020-06-04 21:02:27 -0300

    Add recognition of debt state (eg.: late)

--- src/app/debt/debt-card/debt-card.component.ts
+++ src/app/debt/debt-card/debt-card.component.ts
@@ -6,13 +6,24 @@ import { Debt } from '../../debt.type';
   templateUrl: './debt-card.component.html',
   styleUrls: ['./debt-card.component.css']
 })
-export class DebtCardComponent implements OnInit {
-
+export class DebtCardComponent implements OnInit
+{
   constructor() { }
 
   @Input() debt:Debt;
+  status:number = 0; //0 = Ok, 1 = Próximo do vencimento, 2 = Atraso
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
+
+  	let today = new Date(),
+  		timeDifference = this.debt.deadlineDate - today.getTime();
+
+	if (timeDifference < 0)
+		this.status = 2;
+	else if(timeDifference < 604800000) //Se está a uma semana de vencer, estado de alerta
+		this.status = 1;
+
+	console.log('timeDifference',timeDifference);
   }
-
 }

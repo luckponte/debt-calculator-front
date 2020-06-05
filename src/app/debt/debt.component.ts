fc145d6d16e13c56b93da4a9a3b9df6344f7e0d6
luckponte <luckpontecorreia@gmail.com>
2020-06-04 22:47:32 -0300

    Polish card list styling

--- src/app/debt/debt.component.ts
+++ src/app/debt/debt.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Debt } from '../debt.type';
 
 @Component({
   selector: 'app-debt',
@@ -8,6 +9,32 @@ import { Component, OnInit } from '@angular/core';
 export class DebtComponent implements OnInit {
 
   constructor() { }
+
+  debts:Array<Debt> = [{
+  	id: 0,
+  	deadlineDate:1591659607000,
+  	debtValue:1000,
+  	interestValue:20,
+  	finalValue:1020,
+  	phone:'string',
+  	maxParcels:3
+  },{
+  	id: 1,
+  	deadlineDate:1591054807000,
+  	debtValue:2000,
+  	interestValue:40,
+  	finalValue:2040,
+  	phone:'string',
+  	maxParcels:3
+  },{
+  	id: 2,
+  	deadlineDate:1592264407000,
+  	debtValue:3000,
+  	interestValue:60,
+  	finalValue:3060,
+  	phone:'string',
+  	maxParcels:3
+  }];
 
   ngOnInit(): void {
   }

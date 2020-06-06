e94aa713fb59ae90f53f4a4df53746e9497d2377
luckponte <luckpontecorreia@gmail.com>
2020-06-06 17:12:58 -0300

    Implement http fetch method

--- src/app/debt/debt.service.ts
+++ src/app/debt/debt.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { Debt } from '../debt.type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DebtService {
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private http:HttpClient) { }
+  private apiURL = "http://www.mocky.io/v2/5edbf7ed320000bd7a5d2855";
 
-
+  getDebts():Observable<Debt[]>
+  {
+    return this.http.get<Debt[]>(this.apiURL);
+  }
 }
